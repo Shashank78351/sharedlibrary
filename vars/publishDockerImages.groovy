6f082c21e10ad11672621ac902f635d508a60767
Shashank78351 <shashankmr170@gmail.com>
2024-09-20 00:10:09 +0530

    updated groovy

--- vars/publishDockerImages.groovy
+++ vars/publishDockerImages.groovy
@@ -2,8 +2,8 @@ def call(String backendImage, String frontendImage) {
     withCredentials([usernamePassword(credentialsId: 'Docker', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
         sh """
             docker login -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD} 
-            docker push --all-tags ${backendImage}
-            docker push --all-tags ${frontendImage}
+            docker push  ${backendImage}
+            docker push  ${frontendImage}
         """
     }
 }
