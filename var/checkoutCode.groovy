8a3972eadfb34f33008f7e15567dea4548a29e02
Shashank78351 <shashankmr170@gmail.com>
2024-09-19 22:59:01 +0530

    ok

--- var/checkoutCode.groovy
+++ var/checkoutCode.groovy
@@ -1,4 +1,4 @@
-def call(String branch = 'main', String credentialsId = 'gitlab', String repoUrl) {
+def call(String branch = 'main', String credentialsId = 'Github', String repoUrl) {
     checkout scmGit(branches: [[name: "*/${branch}"]], 
                     extensions: [], 
                     userRemoteConfigs: [[credentialsId: credentialsId, url: repoUrl]]
